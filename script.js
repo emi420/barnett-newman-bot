4357d84cf862f6a6714f11d017c58564a1c0625b
Glitch (hello-webpage) <none>
2019-10-22 21:43:44 +0000

    🌿🌥 Checkpoint ./index.html:7924563/1404 ./style.css:7924563/260 ./script.js:7924563/3868

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+class HTTP {
+  
+  get (url) {
+    var Connect = new XMLHttpRequest();
+    Connect.open("GET", url, false);
+    Connect.setRequestHeader("Content-Type", "text/xml");
+    Connect.send(null);
+    var TheDocument = Connect.responseXML;    
+  }
+}
+
+class App {
+  
+  constructor() {
+    this.getColorButton = document.getElementById("getSomeColorButton");
+    this.init();
+  }
+  
+  init() {
+    this.getColorButton.addEventListener("click", () => {
+      
+    });
+  }
+  
+}
+
+const app = new App();
+app.init();
+
+
