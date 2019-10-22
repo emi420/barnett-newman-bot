9bde241508559d5145e8c4c49f6a534f1321e230
Glitch (hello-webpage) <none>
2019-10-22 21:55:41 +0000

    🚁💘 Checkpoint ./script.js:7924563/5821 ./index.html:7924563/138

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-class HTTP {
-  
-  get (url) {
-    var Connect = new XMLHttpRequest();
-    Connect.open("GET", url, false);
-    Connect.setRequestHeader("Content-Type", "text/xml");
-    Connect.send(null);
-    var TheDocument = Connect.responseXML;    
+const CONFIG = {
+  COLOR_API: "https://www.colourlovers.com/api/",
+  COLOR_API_ENDPOINTS: {
+      PALLETTE_RANDOM: 'palettes/random'
+    }
+};
+
+
+
+class HTTP {  
+  getXML (url) {
+    var conn = new XMLHttpRequest();
+    conn.open("GET", url, false);
+    conn.setRequestHeader("Content-Type", "text/xml");
+    conn.send(null);
+    return conn.responseXML;    
+  }
+}
+
+class ColorAPI {
+  getRandomPalette() {
+      const http = new HTTP();
+      return http.getXML([CONFIG.COLOR_API, CONFIG.PALLETTE_RANDOM].join(''));
   }
 }
 
 class App {
   
   constructor() {
-    this.getColorButton = document.getElementById("getSomeColorButton");
     this.init();
+    this.color = new ColorAPI();
   }
   
   init() {
+    this.getColorButton = document.getElementById("getSomeColorButton");
     this.getColorButton.addEventListener("click", () => {
-      
+      this.getColorButtonHandler();
     });
+  }
+  
+  getColorButtonHandler() {
+    const palette = this.color.getRandomPalette();
+    console.log(palette);
   }
   
 }
